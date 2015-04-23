8473001d55ddaefd2476bb7f8519836f27f4b72b
Jared Sanchez <jaredsanchez@Jareds-MacBook-Pro.local>
2015-04-23 09:32:21 -0700

    Finished second section of homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	
+	$('#sign-right').click(function() {
+		window.location = 'browseScrapbooks.html';
+	})
+
 	// CAROUSEL STUFF STARTS HERE
 	//Implement the "slide to left" when the user clicks on #carousel-next here
 	var currMargin;
@@ -29,4 +32,66 @@ $(document).ready(function() {
 			carousel.css('margin-left', currMargin + 570);
 		}
 	});
+
+
+
+	// HOMEPAGE ARROW ANIMATIONS
+	var animated = false;
+	$(window).bind('scroll', function() {
+		var pos = $('#main-te-container').offset()
+		if ($(window).scrollTop() > pos.top && !animated) {
+			animated = true;
+			// outer arrows
+			$('.outer').animate({
+		        'top': '450%'
+		    }, 1000, function () {
+		    	//remove arrow class, add outer left/right arrow class
+		    	$('#outer-left').addClass('rotate1');
+		    	$('#outer-right').addClass('rotate4');
+		        $('#outer-left').animate({
+		            'left': '170%',
+		            'top': '650%'
+		        }, 1000);
+		        $('#outer-right').animate({
+		            'left': '-150%',
+		            'top': '660%'
+		        }, 1000);
+		    });
+		    $('.outer-bar').animate({
+		    	'height':'350%'},1000,function(){
+		    	$('#bar1').animate({'height':'490%', 'left':'105%', 'top':'300%'},1000);
+		    	$('#bar4').animate({'height':'540%', 'left':'-50%', 'top':'280%'},1000);
+		    });
+
+		    // inner arrows
+		    $('.inner').animate({
+		        'top': '300%'
+		    }, 1000, function () {
+		    	$('#inner-left').addClass('rotate2');
+		    	$('#inner-right').addClass('rotate3');
+		        $('#inner-left').animate({
+		            'left': '97%',
+		            'top': '600%'
+		        }, 1000);
+		        $('#inner-right').animate({
+		            'left': '3%',
+		            'top': '600%'
+		        }, 1000);
+		    });
+		    $('.inner-bar').animate({
+		    	'height':'200%'},1000,function(){
+		    	$('#bar2').animate({'height':'380%', 'left':'68%', 'top':'250%'},1000);
+		    	$('#bar3').animate({'height':'360%', 'left':'25%', 'top':'260%'},1000);
+		    });
+		}
+	});
 })
+
+
+
+
+
+
+
+
+
